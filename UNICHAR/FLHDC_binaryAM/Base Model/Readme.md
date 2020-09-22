4e941fc9fffc921ec2dc53e17a7b36537cc6a86e
WesleyHsieh <b06901053@ntu.edu.tw>
2020-09-22 19:38:50 +0800

    Change the name of UNICHAR to UCIHAR

--- UNICHAR/FLHDC_binaryAM/Base Model/Readme.md
+++
@@ -1,3 +0,0 @@
-# Base_model.py
-In this file, we initialize related parameters for Federated HDC and save them as pickle file. --> We call these setups "Base model".
-Afterward, we partition datasets into NonIID+balanced dataset for K clients.
--- UNICHAR/FLHDC_binaryAM/client_rmtree.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-# delete all files in directories of clients
-for dir in os.listdir():
-    if ("client" in dir) and (os.path.isdir(dir)):
-        shutil.rmtree(dir)
-os.remove("Setup.pickle")
-os.remove("Global_model/flipped_bit.pickle")
-os.remove("Global_model/learning_rate.pickle")
